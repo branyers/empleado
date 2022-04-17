ff0fac47af4346ffe174037eac8da10cd21cfbab
Branyes Nunez <branyernunez@gmail.com>
2022-04-17 09:44:54 -0400

    Learn how to use paginations django
    a

--- applications/persona/urls.py
+++ applications/persona/urls.py
@@ -5,6 +5,6 @@ from .views import All_Empleados, All_Empleados_by_Department
 
 urlpatterns = [
     path('List_employees', All_Empleados.as_view()),
-    path('by_area/<name>/', All_Empleados_by_Department.as_view()),
+    path('by_area/', All_Empleados_by_Department.as_view()),
 
 ]
--- applications/persona/views.py
+++ applications/persona/views.py
@@ -11,13 +11,27 @@ class All_Empleados(ListView):
     template_name = "usuario/all_empleados.html"
     model = Persona
     context_object_name = 'Empleados_list'
-
+    paginate_by = 3
 
 class All_Empleados_by_Department(ListView):
     template_name = "usuario/employees_by_department.html"
-
+    paginate_by = 3
+    """Forma de realiazar una consulta por la barra de busqueda"""
+    # def get_queryset(self):
+    #     area = self.kwargs['name']
+    #     lista = Persona.objects.filter(departament__name=area)
+    #     return lista
+    """Forma de realizar una busqueda por una barra de busqueda utilizando el metodo GET"""
     def get_queryset(self):
-        area = self.kwargs['name']
+        area = self.request.GET.get('kword', '')
         lista = Persona.objects.filter(departament__name=area)
+
         return lista
+
     context_object_name = 'empleados'
+
+
+
+
+
+
